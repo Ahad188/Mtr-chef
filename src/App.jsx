7edb9bfd62d0397d66ab61df07be34fb56aa5582
AhaD <abuahad35188@gmail.com>
2023-05-01 22:08:23 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Components/Header/Header'
+
+function App() {
+   
+
+  return (
+    <>
+       
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+     return (
+          <ul className="menu menu-vertical lg:menu-horizontal bg-base-100 rounded-box">
+  <li><a>Item 1</a></li>
+  <li><a>Item 2</a></li>
+  <li><a>Item 3</a></li>
+</ul>
+     );
+};
+
+export default Header;
