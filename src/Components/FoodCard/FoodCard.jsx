0a563814a258eb2da0e1b99c9b9752cef509d2d0
AhaD <abuahad35188@gmail.com>
2023-05-03 11:06:21 +0600

    tost msg disable to favorate btn

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const FoodCard = ({f1}) => {
-     const {strMeal,strMealThumb} = f1
+     const {strMeal,strMealThumb} = f1;
+     const [dis, setDis] = useState(false)
+     const favorite =()=>{
+          toast('this is my Favorite food')
+          setDis(true)
+     }
      return (
           <div className="card w-96 bg-base-100 shadow-xl">
   <figure className="px-10 pt-10">
@@ -11,8 +18,12 @@ const FoodCard = ({f1}) => {
     <h2 className="card-title">{strMeal}</h2>
      
     <div className="card-actions">
-      <button className="btn">Favorite</button>
+      <button disabled={dis} onClick={()=>{
+          favorite()
+     
+      }}  className="btn">Favorite</button>
     </div>
+    <ToastContainer></ToastContainer>
   </div>
 </div>
      );
